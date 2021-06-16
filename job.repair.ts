615c95fef00ca1b9982b50ec8aed544efaf544f1
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2021-06-16 15:11:15 +0200

    fixed the repair creep
    it will now collect energy if needed

--- job.repair.ts
+++ job.repair.ts
@@ -37,6 +37,11 @@ var RoleRepair: any = {
                     }
                 }
             }
+        } else {
+            const sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
         }
     }
 }
