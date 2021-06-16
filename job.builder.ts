0e76ae28e628830f510b842642a409b01732362f
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2021-06-16 15:50:41 +0200

    when a builder has nothing to do it will act as a havester

--- job.builder.ts
+++ job.builder.ts
@@ -18,9 +18,24 @@ var roleBuilder: any = {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
+            } else {
+                const targets: AnyStructure[] = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure: Structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+                });
+
+                if (targets.length > 0) {
+                    if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                } else {
+                    // make sure they are not blocking the resource collection
+                    creep.moveTo(Game.flags["harvester_idle"]);
+                }
             }
-        }
-        else {
+        } else {
             const sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
