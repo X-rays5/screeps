33ec8533649e7d4903c67470274dc70efe579277
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2021-06-16 14:26:38 +0200

    made it so the havester will now move away from a energy source when done

--- job.harvest.ts
+++ job.harvest.ts
@@ -9,13 +9,24 @@ var roleHarvester: any = {
             }
         }
         else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
+            const targets: AnyStructure[] = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure: Structure) => {
                     return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
                         structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
             });
+
             if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            } else {
+                // make sure they are not blocking the resource collection
+                const target: AnyStructure[] = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure: Structure) => {
+                        return (structure.structureType == STRUCTURE_SPAWN);
+                    }
+                });
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
