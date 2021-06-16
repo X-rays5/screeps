85c40cefba0d6ebbce7c53fb7233dbf0902b4b7e
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2021-06-17 00:41:47 +0200

    fixed a bug which would cause the upgraders to only carry 2 energy

--- job.upgrade.ts
+++ job.upgrade.ts
@@ -1,16 +1,22 @@
 var RoleUpgrade: any = {
     run: function(creep: Creep) {
-        if(creep.store[RESOURCE_ENERGY] == 0) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
+        if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.upgrading = false;
         }
-        else {
+        if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+            creep.memory.upgrading = true;
+        }
+
+        if (creep.memory.upgrading) {
             // @ts-ignore
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 const {controller}: any = creep.room;
                 creep.moveTo(controller);
+            }
+        } else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
             }
         }
     },
