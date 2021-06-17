b13dec36bed0ff71a7008a28f278e73e07a64513
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2021-06-17 16:04:10 +0200

    job and upkeep handlers

---
+++ utility.jobs.ts
@@ -0,0 +1,27 @@
+const JobHarvest = require('job.harvest');
+const JobUpgrade = require('job.upgrader');
+
+const job_handlers = new Map(
+    [
+        ["harvest", JobHarvest],
+        ["upgrade", JobUpgrade]
+    ])
+
+module.exports = {
+    DoJobs: async function() {
+        for (const name in Game.creeps) {
+            const creep = Game.creeps[name];
+            if (job_handlers.has(creep.memory.job)) {
+                job_handlers.get(creep.memory.job).run(creep);
+            } else {
+                console.log(`Unkown job ${creep.memory.job}`);
+            }
+        }
+    },
+
+    DoUpkeep: async function() {
+        job_handlers.forEach((value, key) => {
+            value.upkeep();
+        })
+    }
+}
