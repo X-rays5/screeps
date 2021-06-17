5aa00cf934301cac4abfda91bc291fa501f464c3
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2021-06-17 16:02:15 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 tsc --build
 cd build
 cp -R ./. C:/Users/X-ray/AppData/Local/Screeps/scripts/localhost___21025/default
+cp -R ./. C:/Users/X-ray/AppData/Local/Screeps/scripts/127_0_0_1___21025/default
