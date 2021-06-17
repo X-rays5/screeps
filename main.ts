021fa3f0a9e1dcd810f5e5543b789488e839f2c0
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2021-06-17 16:06:35 +0200

    main function ready for basic functioning

--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
 const utility = require('utility');
 const utility_tasks = require('utility.tasks');
+const utility_jobs = require('utility.jobs');
 
 let init = false;
 
 module.exports.loop = function () {
     if (!init) {
-        init = !init;
         utility_tasks.RunEvery(30, utility.CleanMemory);
+        utility_tasks.RunEvery(10, utility.SpawnQueueTick);
+        utility_tasks.RunEvery(10, utility_jobs.DoUpkeep);
+        init = !init;
     }
     console.log(`--- start of tick ${Game.time} ---`);
-    utility.TasksTick();
-
-
+    utility.TasksTick().catch((reason: any) => {
+        console.log(`Tasks error ${reason}`);
+    });
+    utility_jobs.DoJobs().catch((reason: any) => {
+        console.log(`Jobs error ${reason}`);
+    });
     console.log(`--- end of tick ${Game.time} ---`);
 }
