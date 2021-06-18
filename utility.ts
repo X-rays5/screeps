e8c87a7ae30de680b7cb328f31cbf8a1f541206d
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2021-06-18 19:45:33 +0200

    creep spawn queu system

--- utility.ts
+++ utility.ts
@@ -1,3 +1,7 @@
+interface SpawnQueueItem {
+    BodyParts: BodyPartConstant[],
+    memory: CreepMemory,
+}
 
 module.exports = {
     CleanMemory: async function () {
@@ -6,6 +10,34 @@ module.exports = {
                 delete Memory.creeps[name];
                 console.log(`Cleanup cleared memory of ${name}`)
             }
+        }
+    },
+
+
+    SpawnQueueTick: async function() {
+        console.log("spawn tick");
+        for (const room in Game.rooms) {
+            const spawns = Game.rooms[room].find(FIND_MY_SPAWNS, {
+                filter: (spawn) => {
+                    return (spawn.memory.spawn_queue.length > 0);
+                }
+            });
+
+            if (spawns.length > 0) {
+                const creep = spawns[0].memory.spawn_queue;
+                spawns[0].spawnCreep(creep[0].BodyParts, `${Game.time}_${creep[0].memory.job}`, {memory: creep[0].memory});
+                console.log(`Spawning ${Game.time}_${creep[0].memory.job}`);
+            }
+        }
+    },
+
+    SpawnCreep: async function(room: string, BodyParts: BodyPartConstant[], memory: CreepMemory) {
+        const spawns = Game.rooms[room].find(FIND_MY_SPAWNS);
+        if (spawns.length > 0) {
+            const spawn = spawns[0];
+            console.log(spawn);
+            spawn.memory.spawn_queue.push({BodyParts, memory});
+            console.log(`Added creep to spawn list\n Room: ${room}, BodyParts: ${BodyParts}, Job: ${memory.job}`);
         }
     },
 
@@ -31,7 +63,7 @@ module.exports = {
     TasksTick: async function() {
         let task = require('utility.tasks')
 
-        task.RunTasks();
-        task.RunDelayedTasks();
+        //task.RunTasks();
+        await task.RunDelayedTasks();
     }
 }
