25a59417dff6ee3fb42ac948d37f94fe2b3c99fd
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2021-06-23 02:25:43 +0200

    moved script init

--- main.ts
+++ main.ts
@@ -2,16 +2,7 @@ const utility = require('utility');
 const utility_tasks = require('utility.tasks');
 const utility_jobs = require('utility.jobs');
 
-let init = false;
-
 module.exports.loop = function () {
-    if (!init) {
-        utility_tasks.RunEvery(30, utility.CleanMemory);
-        utility_tasks.RunEvery(10, utility.SpawnQueueTick);
-        utility_tasks.RunDelayed(10, () => {utility.SpawnQueueTick().catch()});
-        //utility_tasks.RunEvery(10, utility_jobs.DoUpkeep);
-        init = !init;
-    }
     console.log(`--- start of tick ${Game.time} ---`);
     utility.TasksTick().catch((reason: any) => {
         console.log(`Tasks error ${reason}`);
@@ -21,3 +12,8 @@ module.exports.loop = function () {
     });
     console.log(`--- end of tick ${Game.time} ---`);
 }
+
+// setup tasks
+utility_tasks.RunEvery(1, utility.CleanMemory).catch((error: any) => {console.log(error);});
+utility_tasks.RunEvery(1, utility.SpawnQueueTick).catch((error: any) => {console.log(error);});
+utility_tasks.RunEvery(10, utility_jobs.DoUpkeep).catch((error: any) => {console.log(error);});
